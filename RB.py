efbf05ead3e10fa9c6c80453e532fe212c3b04b5
Isabel Solano <misolanob@gmail.com>
2023-02-05 18:00:57 -0600

    Bayesian Net imput reading p1

--- RB.py
+++ RB.py
@@ -1,11 +1,33 @@
 import pandas as pd
+from decimal import *
 
-class RB():
+class RB:
     def __init__(self, input):
-        self.data
+        self.data = {}
 
         self.genRB(input)
 
+        print(self.data)
+
 
     def genRB(self, input):
-        0
+        
+        df = pd.read_csv(input, sep = "=", header = None)
+
+        for x in df[0]:
+            print(x)
+
+        for x in df[1]:
+            print(x)
+
+        for i in range(len(df[0])):
+            self.data[
+                self.getEvent(df[0].__getitem__(i))
+            ] = df[1].__getitem__(i)
+
+    def getEvent(self, E):
+        E_temp = str(E)
+        E_temp = E_temp.replace("P(", "")
+        E_temp = E_temp.replace(")", "")
+
+        return E_temp
---
+++ main_tests.py
@@ -0,0 +1,5 @@
+from RB import RB
+
+input = "./Input/red_1.txt"
+
+red = RB(input)
