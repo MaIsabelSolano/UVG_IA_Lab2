df77f4c03f7c0ca8a0d0f15be5e40502e516d949
Ale Gomez <gom20347@uvg.edu.gt>
2023-02-07 21:02:58 -0600

    fixed name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Lab2 - IA",
+    name="Lab2_IA",
     version="0.1",
     description="Laboratorio 2 - Inteligencia Artificial",
     packages=find_packages(),
